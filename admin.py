954fcc40c27d5c9f70095b04a68c5b552b538f53
free_fish <kuailehailiyu@gmail.com>
2016-04-26 22:27:26 +0800

    blog 17

--- admin.py
+++ admin.py
@@ -1,5 +1,7 @@
 from flask.ext.admin import Admin
 from flask.ext.admin.contrib.sqla import ModelView
+from wtforms.fields import SelectField
+from wtforms.fields import PasswordField
 
 from app import app, db
 from models import Entry, Tag, User
@@ -15,13 +17,42 @@ class EntryModelView(ModelView):
         'status': _status_choices,
     }
 
+    column_filters = [
+        'status', User.name, User.email, 'created_timestamp'
+    ]
+
     column_list = [
         'title', 'status', 'author', 'tease', 'tag_list', 'created_timestamp'
     ]
+    
+    column_searchable_list = ['title', 'body']
     column_select_related_list = ['author']
 
+    form_args = {
+        'status': {'choices': _status_choices, 'coerce': int},
+    }
+    form_column = ['title', 'body', 'status', 'author', 'tags']
+    form_overrides = {'status': SelectField}
+    form_ajax_refs = {
+        'author': {
+            'fields': (User.name, User.email),
+        }
+    }
+
 class UserModelView(ModelView):
+    column_filters = ('email', 'name', 'active')
     column_list = ['email', 'name', 'active', 'created_timestamp']
+    column_searchable_list = ['email', 'name']
+
+    form_columns = ['email', 'password', 'name', 'active']
+    form_extra_fields = {
+        'password': PasswordField('New password'),
+    }
+
+    def on_model_change(self, form, model, is_created):
+        if form.password.data:
+            model.password_hash = User.make_password(form.password.data)
+        return super(UserModelView, self).on_model_change(form, model, is_created)
  
 admin = Admin(app, 'Blog BackEnd')
 admin.add_view(EntryModelView(Entry, db.session))
